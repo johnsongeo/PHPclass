5c58ee182d49f17d461f31cbe31d3f556b91cb75
Georgia <700187227@fvtc.edu>
2018-02-07 17:54:49 -0600

    Message

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Class - Georgia Johnson</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyles.css">
+</head>
+<body>
+<div class="page-wrap">
+    <header class="site-header">
+        <h1>PHP Class - Georgia Johnson</h1>
+    </header>
+    <div class="flex-box">
+        <nav class="main-nav">
+            <ul>
+                <li class="menu-item"><a href="/">Home</a></li>
+                <li class="menu-item"><a href="#">Loops</a></li>
+                <li class="menu-item"><a href="#">Strings</a></li>
+            </ul>
+        </nav>
+        <section class="main-content">
+            <div class="img">picture</div>
+            <h2>Hello!</h2>
+            <div class="content">
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when
+                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+                    of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when
+                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+                    of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+            </div>
+        </section>
+
+        <aside class="right-sidebar">Sidebar</aside>
+    </div> <!--/flex-box-->
+    <footer>
+        <div>Learn PHP <sup>&copy;</sup> 2018</div>
+    </footer>
+
+</div> <!--/page-wrap -->
+
+
+</body>
+</html>
